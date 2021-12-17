50fe4a2611565c9b6ca5341287ffd2d972ae738a
LayV <myakinb@mail.ru>
2021-12-17 23:00:05 +0300

    last commit

--- TestGuru/db/seeds.rb
+++ TestGuru/db/seeds.rb
@@ -17,25 +17,25 @@ categories = Category.create!([
 
 tests = Test.create!([
 
-                       { title: 'Initialization', level: 3, author_id: users[2].id, category_id: categories[0].id },
-                       { title: 'Pointers', level: 2, author_id: users[0].id, category_id: categories[1].id },
-                       { title: 'Printing', author_id: users[1].id, category_id: categories[0].id }
+                       { title: 'Initialization', level: 3, author_id: users[2], category_id: categories[0] },
+                       { title: 'Pointers', level: 2, author_id: users[0], category_id: categories[1] },
+                       { title: 'Printing', author_id: users[1], category_id: categories[0] }
 
                      ])
 
 questions = Question.create!([
 
-                               { title: 'How to announce pointer of int x?', test_id: tests[1].id },
-                               { title: 'How to initialize a variable named m with value 5?', test_id: tests[0].id },
-                               { title: "How to print a massive called 'A' in line?", test_id: tests[2].id }
+                               { title: 'How to announce pointer of int x?', test_id: tests[1] },
+                               { title: 'How to initialize a variable named m with value 5?', test_id: tests[0] },
+                               { title: "How to print a massive called 'A' in line?", test_id: tests[2] }
 
                              ])
 
 Answer.create!([
 
-                 { title: "int x;\nint *p = &x;", question_id: questions[0].id },
-                 { title: 'm = 5', question_id: questions[1].id },
-                 { title: 'print A', question_id: questions[2].id }
+                 { title: "int x;\nint *p = &x;", question_id: questions[0] },
+                 { title: 'm = 5', question_id: questions[1]},
+                 { title: 'print A', question_id: questions[2] }
 
                ])
 
