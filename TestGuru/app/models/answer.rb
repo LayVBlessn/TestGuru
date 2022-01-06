91dc4d22653329c1eaf16c0819b06b5121f2d995
LayV <myakinb@mail.ru>
2022-01-06 14:14:45 +0300

    hope last

--- TestGuru/app/models/answer.rb
+++ TestGuru/app/models/answer.rb
@@ -4,10 +4,11 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :title, presence: true
-
+  validate :validate_answers_number
+  
   scope :correct, -> { where(correct: true) }
 
   def validate_answers_number
-    errors.add :base if question.answers.count > 3
+    errors.add :base if question.answers.count >= 4
   end
 end
